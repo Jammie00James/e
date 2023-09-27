5d5a7165647ff4e56d93e5226b6989285b5ca38c
Osayomwanbor Omoeiyekwen James <112559077+Jammie00James@users.noreply.github.com>
2023-09-27 18:36:00 +0100

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -7,6 +7,7 @@ STORAGE_DIR=/opt/render/project/.render
 if [[ ! -d $STORAGE_DIR/chrome ]]; then
   echo "...Downloading Chrome"
   mkdir -p $STORAGE_DIR/chrome
+  cp chromedriver $STORAGE_DIR/chrome
   cd $STORAGE_DIR/chrome
   wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
   dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
